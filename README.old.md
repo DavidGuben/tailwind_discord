4fc55a965a74fafb5647f4152e7d9b65b4fe1c6f
David Guben <davidjg6@gmail.com>
2022-04-18 19:14:15 -0400

    init commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# tailwind_discord
+A discord clone created using Tailwind CSS &amp; MERN Stack
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div className="bg-gray-700 text-white min-h-screen flex items-center justify-center">
+        <img src="/bird-pic.jpg" alt="bird"/>
+      </div>
+    );
+  }
